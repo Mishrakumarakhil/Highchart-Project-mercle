cee0024c7d6765810c10ea6059750e209eb3bac6
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-04 02:26:36 +0530

    updte title

--- mercle-highchart-assignment/src/App.js
+++ mercle-highchart-assignment/src/App.js
@@ -4,7 +4,7 @@ import EngagementMessagesOverTime from './components/EngagementMessagesOverTime'
 function App() {
   return (
     <div className="App">
-     <div className="title">EngagementHelper Chart </div>
+     <div className="title">Engagement Helper Over Time Chart </div>
      <div className="chart-conatiner">
       <EngagementMessagesOverTime/>
      </div>
